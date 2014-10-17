12724c5b5447659f78098adc010055051e6a93a0
David Boivin ~Spit <db2_hockey@hotmial.com>
2014-10-16 23:32:35 -0400

    -added the log framework -filled out the main part of the MainFrame

---
+++ StockExchange/src/com/dspit/stockExchange/data/Transaction.java
@@ -0,0 +1,13 @@
+
+
+package com.dspit.stockExchange.data;
+
+/**
+ * A log entry of what the user has done with different 
+ * portfolios.
+ *
+ * @author David Boivin (Spit)
+ */
+public class Transaction {
+
+}
--- StockExchange/src/com/dspit/stockExchange/mainframe/MainFrame.java
+++ StockExchange/src/com/dspit/stockExchange/mainframe/MainFrame.java
@@ -2,17 +2,55 @@
 
 package com.dspit.stockExchange.mainframe;
 
+import com.dspit.stockExchange.data.CompanyList;
+import com.dspit.stockExchange.data.PortfolioList;
+
 /**
  * Main Stock Exchange SubSystem. Runs all GUI and 
  * computational processes
  *
  * @author David Boivin (Spit)
  */
-public class MainFrame {
+public class MainFrame{
+	
+	
+// Members ----------------------------------------------------------------- //
+	
+	private CompanyList mCompanies;
+	private PortfolioList mPortfolios;
+	
+// Constructors ------------------------------------------------------------ //
+	
+	public MainFrame(){
+		mCompanies = new CompanyList();
+		mPortfolios = new PortfolioList();
+	}
+	
+// Public Methods ---------------------------------------------------------- //
+	
+	/**
+	 * Displays a GUI with all the information to start off the investor 
+	 * stock exchange program.
+	 */
+	public void start(){
+		
+	}
+	
+	/**
+	 * Displays the log of all the user's activity during this session
+	 */
+	public void exit(){
+		
+	}
 	
 // MAIN -------------------------------------------------------------------- //
 	
 	public static void main(String[] args){
+		
+		MainFrame exchange = new MainFrame();
+		
+		exchange.start();
+		exchange.exit();
 		
 	}
 }
